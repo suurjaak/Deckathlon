a41abcd0a39b7d4e0dbbff80e70a5013dcb064cb
Erki Suurjaak <erki@lap.ee>
2020-05-19 14:21:10 +0300

    Updated launch.py to be WSGI-compatible.

--- launch.py
+++ launch.py
@@ -1,3 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+WSGI or manual launch entrypoint.
+
+------------------------------------------------------------------------------
+This file is part of Deckathlon - card game website.
+Released under the MIT License.
+
+@author    Erki Suurjaak
+@created   04.05.2020
+@modified  19.05.2020
+------------------------------------------------------------------------------
+"""
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+
 from deckathlon import index
 
-index.run()
+
+app = index.init()
+
+def application(environ, start_response):
+    # Wrapper to set SCRIPT_NAME to actual mount point.
+    environ["SCRIPT_NAME"] = os.path.dirname(environ["SCRIPT_NAME"])
+    if environ['SCRIPT_NAME'] == "/":
+        environ['SCRIPT_NAME'] = ""
+
+    return app(environ,  start_response)
+
+
+if "__main__" == __name__:
+    index.run()
